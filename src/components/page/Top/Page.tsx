101427d742297530644f70a9b1c7348321526b37
Hajime Nakagawa <navyblue0914@icloud.com>
2022-03-07 14:52:32 +0900

    set head

--- src/components/page/Top/Page.tsx
+++ src/components/page/Top/Page.tsx
@@ -8,12 +8,15 @@ export const TopPage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>NextWind</title>
+        <title>daisyUI Slack mock</title>
         <meta
           name="description"
-          content="A template for prototyping with Next.js and Tailwind CSS"
+          content="A mock app of Slack made of daisyUI"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🌼</text></svg>"
+        />
       </Head>
 
       <TopPageView />
